91a36813af79dda87f79a809e995589c3546d7a9
doljae <seok9211@naver.com>
2021-05-06 15:55:46 +0900

    Time: 576 ms (15.86%), Space: 19.5 MB (11.25%) - LeetHub

--- daily-temperatures/daily-temperatures.py
+++ daily-temperatures/daily-temperatures.py
@@ -1,7 +1,5 @@
 from typing import *
 
-from collections import deque
-
 
 class Solution:
     def dailyTemperatures(self, T: List[int]) -> List[int]:
@@ -10,19 +8,11 @@ class Solution:
         stack, answer = [], []
         while new_list:
             cur = new_list.pop()
-            # print(stack, cur)
-            if not answer:
-                answer.append(0)
-                stack.append(cur)
-            else:
-                while stack and stack[-1][1] <= cur[1]:
+            while stack and stack[-1][1] <= cur[1]:
                     stack.pop()
-                if not stack:
-                    answer.append(0)
-                    stack.append(cur)
-                else:
-                    answer.append(stack[-1][0] - cur[0])
-                    stack.append(cur)
-            # print(answer)
-            # print()
+            if not stack:
+                answer.append(0)
+            else:
+                answer.append(stack[-1][0] - cur[0])
+            stack.append(cur)
         return answer[::-1]
