5b57e874f9b83bcf10b99c0ffe0d6bba9ef0de3f
doljae <seok9211@naver.com>
2021-05-10 09:21:42 +0900

    Time: 112 ms (30.93%), Space: 19.7 MB (18.45%) - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,8 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         dict1={}
+        
+        def count(input_string):
+            count_list=[0]*26
+            for char in input_string:
+                count_list[ord(char)-ord("a")]+=1
+            return tuple(count_list)
+            
+        
         for str in strs:
-            temp="".join(sorted(list(str)))
+            temp=count(str)
             if temp not in dict1:
                 dict1[temp]=[str]
             else:
