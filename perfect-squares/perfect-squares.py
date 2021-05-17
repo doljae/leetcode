4f8e574551807cf6966e27c34f54d1f175e5c29b
doljae <seok9211@naver.com>
2021-05-17 13:12:44 +0900

    Time: 1460 ms (67.02%), Space: 15.1 MB (33.11%) - LeetHub

--- perfect-squares/perfect-squares.py
+++ perfect-squares/perfect-squares.py
@@ -1,18 +1,15 @@
-import math
-
-
 class Solution:
     def numSquares(self, n: int) -> int:
-        dp = [0] * (n + 1)
-        for i in range(1, int(math.sqrt(n)) + 1):
-            dp[i * i] = 1
-        for i in range(1, n + 1):
-            if dp[i]:
-                continue
-            else:
-                temp = int(math.sqrt(i))
-                dp[i] = dp[temp] + dp[i - temp]
-                for j in range(1, temp + 1):
-                    dp[i] = min(dp[i], dp[j * j] * (i // (j * j)) + dp[i % (j * j)])
-        # print(dp)
-        return dp[n]
+        q = deque([(n, 0)])
+        set1 = {n}
+        while q:
+            cur_num, cur_level = q.popleft()
+            if cur_num == 0:
+                return cur_level
+            j = 1
+            while cur_num >= j * j:
+                temp = cur_num - (j * j)
+                if temp not in set1:
+                    set1.add(temp)
+                    q.append((temp, cur_level + 1))
+                j += 1
