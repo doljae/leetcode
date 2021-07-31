6759d4ff5d287bed11ade09efa18dae815ba6856
doljae <seok9211@naver.com>
2021-07-31 15:54:47 +0900

    Time: 32 ms (67.75%), Space: 14.2 MB (86.57%) - LeetHub

---
+++ binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.py
@@ -0,0 +1,35 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def bstToGst(self, root):
+        def dfs(cur, val, saved):
+            # print(cur.val)
+            if not cur.left and not cur.right:
+                cur.val += saved
+                return cur.val
+
+            right = 0
+            if cur.right:
+                right = dfs(cur.right, val, saved)
+            # print(f"!{cur.val},{saved},{right}")
+            cur.val += right
+            if not right:
+                cur.val += saved
+            if not saved:
+                saved = cur.val
+            else:
+                saved = max(saved, cur.val)
+            # print(f"!{cur.val},{saved}")
+            left = 0
+            if cur.left:
+                left = dfs(cur.left, cur.val, saved)
+            # print(f"!!{cur.val},{saved},{left}")
+            return max(cur.val, left)
+
+        dfs(root, 0, 0)
+        return root
