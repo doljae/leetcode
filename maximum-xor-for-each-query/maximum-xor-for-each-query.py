9490d4488bf0af5704ac53643924cecc0b323e47
doljae <seok9211@naver.com>
2021-09-04 10:55:46 +0900

    Time: 1088 ms (51.79%), Space: 31.7 MB (68.57%) - LeetHub

--- maximum-xor-for-each-query/maximum-xor-for-each-query.py
+++ maximum-xor-for-each-query/maximum-xor-for-each-query.py
@@ -6,11 +6,13 @@ class Solution:
     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
         answer = []
         target = None
+        
+        maximum_xor = (1 << maximumBit) - 1
         for i in range(len(nums)):
             if target is None:
                 target = reduce(lambda x, y: x ^ y, nums[:len(nums) - i])
             else:
                 target ^= nums[len(nums) - i]
-            answer.append(int("1" * maximumBit, 2) ^ target)
+            answer.append(maximum_xor ^ target)
 
         return answer
