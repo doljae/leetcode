11b92b18f0ce9e4fcf53286d80b89444aa26c76c
doljae <seok9211@naver.com>
2021-09-26 09:59:35 +0900

    Time: 40 ms (34.29%), Space: 14.3 MB (6.55%) - LeetHub

--- valid-perfect-square/valid-perfect-square.py
+++ valid-perfect-square/valid-perfect-square.py
@@ -1,32 +1,8 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
+        odd = 1
+        while num > 0:
+            num -= odd
+            odd += 2
 
-        if num == 1:
-            return True
-
-        max_target = 2
-        while (max_target + 1) * (max_target + 1) <= num:
-            max_target += 1
-
-        cur = num
-        temp = 2
-        answer = []
-        while cur != 1:
-            temp2 = 0
-            while cur % temp == 0:
-                temp2 += 1
-                cur = cur // temp
-
-            answer.append(temp2)
-            temp += 1
-            if temp > max_target:
-                break
-
-        if cur != 1:
-            answer.append(1)
-
-        for item in answer:
-            if item % 2 != 0:
-                return False
-
-        return True
+        return True if num == 0 else False
