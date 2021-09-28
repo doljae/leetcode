8271ad66268658bdd67e43657b73596c18f29293
doljae <seok9211@naver.com>
2021-09-28 12:49:40 +0900

    Time: 212 ms (86.09%), Space: 26.1 MB (12.38%) - LeetHub

--- rotate-array/rotate-array.py
+++ rotate-array/rotate-array.py
@@ -4,15 +4,8 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        stack=nums[::-1]
-        k = k%len(nums)
+        q = deque(nums)
         
-        left,right=len(nums)-k, k 
-        
-        stack=stack[:right][::-1]+stack[right:][::-1]
-        
-        for i in range(len(stack)):
-            nums[i]=stack[i]
-        
-        print(nums)
-        
+        q.rotate(k)
+
+        nums[:] = list(q)
