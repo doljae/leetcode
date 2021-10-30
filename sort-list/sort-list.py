f514d27ac112319c3454fedcc9a5448ab7a5da28
doljae <seok9211@naver.com>
2021-10-30 11:46:08 +0900

    Time: 456 ms (69.98%), Space: 30.1 MB (70.67%) - LeetHub

--- sort-list/sort-list.py
+++ sort-list/sort-list.py
@@ -10,17 +10,40 @@ class ListNode:
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-        if not head:
-            return None
-        trace = []
-        cur = head
-        while cur:
-            trace.append(cur)
-            cur = cur.next
-        trace.sort(key=lambda x: x.val)
+        if not head or not head.next:
+            return head
 
-        for i in range(1, len(trace)):
-            trace[i - 1].next = trace[i]
-            
-        trace[-1].next = None
-        return trace[0]
+        p1, p2 = head, head.next
+
+        while p2 and p2.next:
+            p1 = p1.next
+            p2 = p2.next.next
+
+        right = p1.next
+        p1.next = None
+        left = head
+        left_sorted_list = self.sortList(left)
+        right_sorted_list = self.sortList(right)
+
+        return self.merge(left_sorted_list, right_sorted_list)
+
+    def merge(self, left, right):
+        if not left:
+            return right
+        if not right:
+            return left
+
+        temp = ListNode(0)
+        cur = temp
+
+        while left and right:
+            if left.val >= right.val:
+                cur.next = right
+                right = right.next
+            else:
+                cur.next = left
+                left = left.next
+            cur = cur.next
+
+        cur.next = left if left else right
+        return temp.next
