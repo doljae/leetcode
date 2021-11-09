7b4ea439979b3948ca1827a74049f6127bcd239a
doljae <seok9211@naver.com>
2021-11-09 11:40:43 +0900

    Time: 28 ms (90.95%), Space: 14.4 MB (24.65%) - LeetHub

--- is-subsequence/is-subsequence.py
+++ is-subsequence/is-subsequence.py
@@ -1,18 +1,11 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        dp = [[0] * (len(s) + 1) for _ in range(len(t) + 1)]
-        max_length = 0
-        for i in range(1, len(t) + 1):
-            t1 = t[i - 1]
-            for j in range(1, len(s) + 1):
-                t2 = s[j - 1]
 
-                if t1 == t2:
-                    dp[i][j] = dp[i - 1][j - 1] + 1
+        for char in s:
+            index = t.find(char)
+            if index == -1:
+                return False
 
-                else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+            t = t[index + 1:]
 
-                max_length = max(max_length, dp[i][j])
-        
-        return max_length == len(s)
+        return True
