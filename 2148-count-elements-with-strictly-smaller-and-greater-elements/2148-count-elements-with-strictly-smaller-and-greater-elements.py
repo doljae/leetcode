776cc6d96ddc1f3243f9f8fbe5d14157ad58a977
Seokjae Lee <seok9211@naver.com>
2022-11-12 13:00:37 +0900

    Time: 159 ms (5.27%), Space: 14.4 MB (15.35%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
@@ -2,18 +2,9 @@ from sortedcontainers import SortedDict
 
 class Solution:
     def countElements(self, nums: List[int]) -> int:
-        sd = SortedDict()
         result = 0
         for num in nums:
-            if num in sd:
-                sd[num] += 1
-            else:
-                sd[num] = 1
-        
-        keys = sd.keys()
-        for i in range(1, len(keys)-1):
-            if keys[i-1] < keys[i] < keys[i+1]:
-                result += sd[keys[i]]
+            if min(nums) < num < max(nums):
+                result += 1
         
         return result
-        
