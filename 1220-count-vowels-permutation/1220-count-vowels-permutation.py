45dc0fc8b3eb4a68ba2aaec1b906d90507e0a882
Seokjae Lee <seok9211@naver.com>
2023-01-22 15:44:55 +0900

    Time: 1140 ms (22.47%), Space: 19.3 MB (50.58%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,7 +1,7 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
         board = [[0] * 5 for _ in range(n + 1)]
-
+        modulo = pow(10, 9) + 7
         A, E, I, O, U = 0, 1, 2, 3, 4
         index_map = {A: [E],
                      E: [A, I],
@@ -14,6 +14,6 @@ class Solution:
                 if i == 1:
                     board[i][j] = 1
                 else:
-                    board[i][j] += sum(list(map(lambda x: board[i - 1][x], index_map[j]))) % (pow(10, 9) + 7)
+                    board[i][j] += sum(list(map(lambda x: board[i - 1][x], index_map[j]))) % modulo
 
-        return sum(board[n]) % (pow(10, 9) + 7)
+        return sum(board[n]) % modulo
