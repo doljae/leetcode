76bf85d2aa8e3389e3b5b6c3bd2607367e6c24aa
Seokjae Lee <seok9211@naver.com>
2023-02-26 09:27:39 +0900

    Time: 185 ms (47.06%), Space: 14.4 MB (10.63%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -0,0 +1,25 @@
+from typing import *
+
+
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        height, width = len(grid), len(grid[0])
+        k %= (height * width)
+        board = [[0] * width for _ in range(height)]
+
+        for i in range(height):
+            for j in range(width):
+                row, col = self.shift(i, j, height, width, k)
+                board[row][col] = grid[i][j]
+
+        for item in board:
+            print(item)
+
+        return board
+
+    def shift(self, row, col, height, width, count):
+        col += count
+        a, b = divmod(col, width)
+        row, col = (row + a) % height, b
+
+        return (row, col)
