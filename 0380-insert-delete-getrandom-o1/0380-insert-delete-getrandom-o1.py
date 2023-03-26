24a20c372ce39ab75d29b7c0eb76661e65948d8c
Seokjae Lee <seok9211@naver.com>
2023-03-26 17:02:33 +0900

    Time: 986 ms (5.06%), Space: 61.3 MB (76.00%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -0,0 +1,22 @@
+import random
+
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.set = set()
+
+    def insert(self, val: int) -> bool:
+        if val in self.set:
+            return False
+        self.set.add(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.set:
+            return False
+        self.set.remove(val)
+        return True
+
+    def getRandom(self) -> int:
+        return random.sample(self.set, 1)[0]
