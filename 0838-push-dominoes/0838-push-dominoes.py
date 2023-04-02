5cd5af55fcdba6566c48f5e2dafa7fef725e854a
Seokjae Lee <seok9211@naver.com>
2023-04-02 11:36:00 +0900

    Time: 190 ms (86.77%), Space: 17.2 MB (60.05%) - LeetHub

---
+++ 0838-push-dominoes/0838-push-dominoes.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        stack, result = [], []
+        r_cnt = False
+
+        for domino in dominoes:
+            if domino == ".":
+                stack.append(domino)
+            elif domino == "R":
+                if not r_cnt:
+                    result += stack
+                    r_cnt = True
+                    stack = [domino]
+                else:
+                    size = len(stack)
+                    result += ["R"] * size
+                    stack = [domino]
+            elif domino == "L":
+                if not r_cnt:
+                    size = len(stack)
+                    result += ["L"] * (size + 1)
+                    stack = []
+                else:
+                    size = len(stack) + 1
+                    if size % 2 == 0:
+                        result += ["R"] * (size // 2)
+                        result += ["L"] * (size // 2)
+                    else:
+                        result += ["R"] * (size // 2)
+                        result.append(".")
+                        result += ["L"] * (size // 2)
+                    stack = []
+                    r_cnt = False
+            # print(f"stack:{stack}")
+            # print(f"result:{result}")
+            # print()
+
+        if stack and stack[0] == "R":
+            stack = ["R"] * len(stack)
+
+        return "".join(result + stack)
