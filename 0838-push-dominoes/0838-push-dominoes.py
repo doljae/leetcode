7a311fe33df640f270fd9a50e0cdc76fb191c64a
Seokjae Lee <seok9211@naver.com>
2023-04-02 11:38:25 +0900

    Time: 179 ms (88.66%), Space: 16.9 MB (63.70%) - LeetHub

--- 0838-push-dominoes/0838-push-dominoes.py
+++ 0838-push-dominoes/0838-push-dominoes.py
@@ -14,27 +14,22 @@ class Solution:
                 if not r_cnt:
                     result += stack
                     r_cnt = True
-                    stack = [domino]
                 else:
                     size = len(stack)
                     result += ["R"] * size
-                    stack = [domino]
+                stack = [domino]
             elif domino == "L":
                 if not r_cnt:
                     size = len(stack)
                     result += ["L"] * (size + 1)
-                    stack = []
                 else:
                     size = len(stack) + 1
-                    if size % 2 == 0:
-                        result += ["R"] * (size // 2)
-                        result += ["L"] * (size // 2)
-                    else:
-                        result += ["R"] * (size // 2)
+                    result += ["R"] * (size // 2)
+                    if size % 2 != 0:
                         result.append(".")
-                        result += ["L"] * (size // 2)
-                    stack = []
+                    result += ["L"] * (size // 2)
                     r_cnt = False
+                stack = []
 
         if stack and stack[0] == "R":
             stack = ["R"] * len(stack)
