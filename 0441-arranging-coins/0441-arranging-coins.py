33c255ff8113cf85e52786dd9f2d16b5d9a0a766
Seokjae Lee <seok9211@naver.com>
2023-04-08 12:13:03 +0900

    Time: 39 ms (68.85%), Space: 13.8 MB (43.73%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,3 +1,14 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        return int((2 * n + 0.25) ** 0.5 - 0.5)
+        left, right = 0, n
+        while left <= right:
+            k = (left + right) // 2
+            temp = k * (k + 1) // 2
+            if temp == n:
+                return k
+            elif n < temp:
+                right = k - 1
+            else:
+                left = k + 1
+        
+        return right
