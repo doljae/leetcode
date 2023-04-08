c0843358f6222226e1821ef5d8833448a631dd0e
Seokjae Lee <seok9211@naver.com>
2023-04-08 12:45:42 +0900

    Time: 947 ms (52.41%), Space: 14.2 MB (14.04%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,26 +1,27 @@
-class Solution:
-    def fourSum(self, nums, target):
-        def findNsum(nums, target, N, result, results):
-            if len(nums) < N or N < 2 or target < nums[0]*N or target > nums[-1]*N:  # early termination
-                return
-            if N == 2: # two pointers solve sorted 2-sum problem
-                l,r = 0,len(nums)-1
+class Solution:  # 1084 ms, faster than 37.26%
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        def dfs(l, r, k, target, path, out):  # [l, r] inclusive
+            if k == 2:
                 while l < r:
-                    s = nums[l] + nums[r]
-                    if s == target:
-                        results.append(result + [nums[l], nums[r]])
-                        l += 1
-                        while l < r and nums[l] == nums[l-1]:
-                            l += 1
-                    elif s < target:
-                        l += 1
+                    if nums[l] + nums[r] == target:
+                        out.append(path + [nums[l], nums[r]])
+                        while l+1 < r and nums[l] == nums[l+1]: l += 1  # Skip duplicate nums[l]
+                        l, r = l + 1, r - 1
+                    elif nums[l] + nums[r] > target:
+                        r -= 1  # Decrease sum
                     else:
-                        r -= 1
-            else: # recursively reduce N
-                for i in range(len(nums)-N+1):
-                    if i == 0 or (i > 0 and nums[i-1] != nums[i]):
-                        findNsum(nums[i+1:], target-nums[i], N-1, result+[nums[i]], results)
+                        l += 1  # Increase sum
+                return
 
-        results = []
-        findNsum(sorted(nums), target, 4, [], results)
-        return results
+            while l < r:
+                dfs(l+1, r, k - 1, target - nums[l], path + [nums[l]], out)
+                while l+1 < r and nums[l] == nums[l+1]: l += 1  # Skip duplicate nums[i]
+                l += 1
+
+        def kSum(k):  # k >= 2
+            ans = []
+            nums.sort()
+            dfs(0, len(nums)-1, k, target, [], ans)
+            return ans
+
+        return kSum(4)
