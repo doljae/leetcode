a9d49c3528069d014852064016cc4d80ffb5f723
Seokjae Lee <seok9211@naver.com>
2023-04-16 15:59:00 +0900

    Time: 1165 ms (28.71%), Space: 25.1 MB (5.25%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -7,18 +7,18 @@ from typing import *
 
 class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
-        top, bottom = defaultdict(set), defaultdict(set)
+        top_map, bottom_map = defaultdict(set), defaultdict(set)
 
         for index in range(len(tops)):
-            top[tops[index]].add(index)
-            bottom[bottoms[index]].add(index)
+            top_map[tops[index]].add(index)
+            bottom_map[bottoms[index]].add(index)
 
         result = float('inf')
-        for key in top:
-            u = top[key].union(bottom[key])
-            i = top[key].intersection(bottom[key])
+        for key in top_map:
+            u = top_map[key].union(bottom_map[key])
+            i = top_map[key].intersection(bottom_map[key])
             if len(u) == len(tops):
-                result = min(result, min(len(top[key] - i), len(bottom[key] - i)))
+                result = min(result, min(len(top_map[key] - i), len(bottom_map[key] - i)))
 
         if result == float('inf'):
             return -1
