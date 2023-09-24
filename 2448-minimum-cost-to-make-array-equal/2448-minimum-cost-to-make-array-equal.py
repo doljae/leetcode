e385c1e8453bd3939de4200ed2289860e9fa2f01
Seokjae Lee <seok9211@naver.com>
2023-09-24 17:49:48 +0900

    Time: 338 ms (89.09%), Space: 37.3 MB (35.63%) - LeetHub

---
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
@@ -0,0 +1,23 @@
+from typing import *
+
+
+class Solution:
+    def minCost(self, nums: List[int], cost: List[int]) -> int:
+        if len(set(nums)) == 1:
+            return 0
+
+        board = []
+        for i in range(len(nums)):
+            board.append((nums[i], cost[i]))
+        board.sort()
+
+        total, cnt = sum(cost), 0
+        target = 0
+        for item in board:
+            num, weight = item
+            cnt += weight
+            if cnt > total // 2:
+                target = num
+                break
+
+        return sum(abs(num - target) * weight for num, weight in board)
