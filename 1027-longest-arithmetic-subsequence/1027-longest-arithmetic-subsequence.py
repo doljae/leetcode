84b025cb65ca28f2313dd935b831f62d286eb99c
Seokjae Lee <seok9211@naver.com>
2023-10-01 21:59:35 +0900

    Time: 2317 ms (50.91%), Space: 33.9 MB (72.77%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.py
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.py
@@ -4,12 +4,10 @@ from typing import List
 class Solution:
     def longestArithSeqLength(self, nums: List[int]) -> int:
 
-        n = len(nums)
-        if n <= 2:
-            return n
+ 
 
         result = 2
-        board = [{} for _ in range(n)]
+        board = [{} for _ in range(len(nums))]
 
         for i in range(len(nums)):
             for j in range(i):
