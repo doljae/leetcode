77fd183b017ab6aa6d2bdcc577843072bd606ffc
Seokjae Lee <seok9211@naver.com>
2023-11-05 21:21:31 +0900

    Time: 36 ms (76.97%), Space: 16.2 MB (72.16%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,27 +1,11 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        result = 0
-        for i in range(1, n + 1):
-            temp, board = 1, None
-            if n % i == 0:
-                board = [i] * (n // i)
-            else:
-                board = [i] * (n // i) + [n % i]
-
-            if len(board) >= 2:
-                for item in board:
-                    temp *= item
-                # print(board)
-                result = max(result, temp)
-
-            temp = 1
-            if len(board) > 2:
-                one = board[-1]
-                board = board[:-1]
-                board[-1] += one
-                for item in board:
-                    temp *= item
-                # print(board)
-                result = max(result, temp)
-
-        return result
+        if n == 2:
+            return 1
+        if n == 3:
+            return 2
+        if n % 3 == 0:
+            return 3 ** (n // 3)
+        if n % 3 == 1:
+            return 3 ** ((n // 3) - 1) * 4
+        return 3 ** (n // 3) * 2
