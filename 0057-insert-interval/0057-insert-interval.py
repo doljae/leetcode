8809532ae01a7d5a5a661ce388c3794e395eefea
Seokjae Lee <seok9211@naver.com>
2024-01-06 18:41:16 +0900

    Time: 84 ms (49.54%), Space: 21.8 MB (13.56%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -4,52 +4,30 @@ from typing import List
 
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        board = []
         if not intervals:
             return [newInterval]
+
+        board = [(newInterval[0], "in"), (newInterval[1], "out")]
         for interval in intervals:
             start, end = interval
-            board.append((start, "start"))
-            board.append((end, "end"))
-        new_interval_start, new_interval_end = newInterval
-        new_interval_start_flag = False
-        new_interval_end_flag = False
-        # print(board)
-        if board and board[0][0] >= new_interval_start:
-            board = [(new_interval_start, "start")] + board
-            new_interval_start_flag = True
-            # print(1)
-        for i in range(len(board) - 1):
-            if new_interval_start_flag is False and board[i][0] <= new_interval_start <= board[i + 1][0]:
-                board = board[:i] + [(new_interval_start, "start")] + board[i:]
-                new_interval_start_flag = True
-                # print(1)
-            if new_interval_end_flag is False and board[i][0] <= new_interval_end <= board[i + 1][0]:
-                board = board[:i] + [(new_interval_end, "end")] + board[i:]
-                new_interval_end_flag = True
-                # print(2)
-        if not new_interval_start_flag:
-            board.append((new_interval_start, "start"))
-        if not new_interval_end_flag:
-            board.append((new_interval_end, "end"))
-        board.sort(key=lambda x: (x[0], (x[1] == "end")))
-        # print(board)
+            board.append((start, "in"))
+            board.append((end, "out"))
+
+        board.sort(key=lambda x: (x[0], (x[1] == "out")))
 
         result = []
         temp = deque([])
         start_cnt, end_cnt = 0, 0
         while board:
-            # print(temp)
             cur_val, cur_flag = board.pop()
-            if cur_flag == "start":
+            if cur_flag == "in":
                 temp.appendleft(cur_val)
                 start_cnt += 1
             else:
                 if temp and cur_val != temp[0] and start_cnt == end_cnt:
                     result.append([temp[0], temp[-1]])
+                    temp = deque([cur_val])
                     start_cnt, end_cnt = 0, 1
-                    temp = deque([])
-                    temp.appendleft(cur_val)
                 elif temp and cur_val == temp[0]:
                     end_cnt += 1
                 else:
