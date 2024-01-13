e5a784f464c8d13eeb40b6a1619779106f8557e2
Seokjae Lee <seok9211@naver.com>
2024-01-13 11:06:11 +0900

    Time: 550 ms (99.31%), Space: 23.6 MB (37.74%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -7,4 +7,4 @@ class Solution:
         return sorted(points, key=lambda x: self.euclideanDistance(x[0], x[1], 0, 0))[:k]
 
     def euclideanDistance(self, x1, y1, x2, y2):
-        return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+        return (x1 - x2) ** 2 + (y1 - y2) ** 2
