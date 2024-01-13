2937d7d1572bd304996a10d018e9ae36cacf25b7
Seokjae Lee <seok9211@naver.com>
2024-01-13 10:51:21 +0900

    Time: 50 ms (91.25%), Space: 17.5 MB (19.29%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        q = deque([])
+        visited = set()
+        result = 0
+        for char in s:
+            if not q:
+                q.append(char)
+                visited.add(char)
+            else:
+                if char not in visited:
+                    q.append(char)
+                    visited.add(char)
+                else:
+                    while q and q[0] != char:
+                        visited.remove(q.popleft())
+                    if q:
+                        visited.remove(q.popleft())
+                    q.append(char)
+                    visited.add(char)
+            result = max(result, len(q))
+
+        return result
