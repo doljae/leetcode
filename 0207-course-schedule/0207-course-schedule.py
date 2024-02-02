5c1d6a1d8b36d939ad3bf398b849178984f36549
Seokjae Lee <seok9211@naver.com>
2024-02-02 23:41:41 +0900

    Time: 90 ms (74.09%), Space: 18.4 MB (76.45%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -18,28 +18,24 @@ class Solution:
             indegree[b] += 1
             if a not in indegree:
                 indegree[a] = 0
-        # print(graph)
-        visited = set()
+                
         q = deque([])
         for key in indegree:
             if indegree[key] == 0:
                 q.append(key)
-        # print(indegree)
-        # print(q)
+
         if not q:
             return False
 
         while q:
             cur = q.popleft()
-            visited.add(cur)
             for adj in graph[cur]:
                 indegree[adj] -= 1
                 if indegree[adj] == 0:
                     q.append(adj)
-            # print(indegree)
 
         for key in indegree:
             if indegree[key] != 0:
                 return False
+            
         return True
-        # return len(visited) == numCourses
