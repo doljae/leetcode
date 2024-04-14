417c74e4aeed517b0de2d523006a8bd8273b8709
Seokjae Lee <seok9211@naver.com>
2024-04-14 18:14:10 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        board = []
+
+        char_list = deque(list(s))
+
+        while char_list:
+            char = char_list.popleft()
+            if char.isdigit():
+                temp = [char]
+                while char_list and char_list[0].isdigit():
+                    temp.append(char_list.popleft())
+                
+                int_char = int("".join(temp))
+                if board and board[-1] == "-":
+                    board.pop()
+                    board.append(-int_char)
+                else:
+                    board.append(int_char)
+            elif char == "+":
+                continue
+            elif char == "-":
+                board.append(char)
+            elif char == " ":
+                continue
+            elif char == "(":
+                board.append(char)
+            elif char == ")":
+                temp = 0
+                while board and board[-1] != "(":
+                    temp += board.pop()
+                board.pop()
+                board.append(temp)
+
+        return sum(board)
