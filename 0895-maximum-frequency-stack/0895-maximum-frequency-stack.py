b616d8b4a50c66d9ca8fbb073b2eb7b53af204c0
Seokjae Lee <seok9211@naver.com>
2024-05-18 22:47:39 +0900

    Time: 229 ms (75.54%), Space: 25.1 MB (89.92%) - LeetHub

--- 0895-maximum-frequency-stack/0895-maximum-frequency-stack.py
+++ 0895-maximum-frequency-stack/0895-maximum-frequency-stack.py
@@ -1,23 +1,23 @@
-from collections import Counter, defaultdict
+from collections import defaultdict
 
 
 class FreqStack:
 
     def __init__(self):
-        self.frequency = Counter()
-        self.map = defaultdict(list)
         self.max_frequency = 0
+        self.board = defaultdict(list)
+        self.counter = defaultdict(int)
 
     def push(self, val: int) -> None:
-        self.frequency[val] += 1
-        self.max_frequency = max(self.max_frequency, self.frequency[val])
-        self.map[self.frequency[val]].append(val)
+        self.counter[val] += 1
+        self.board[self.counter[val]].append(val)
+        self.max_frequency = max(self.max_frequency, self.counter[val])
 
     def pop(self) -> int:
-        target = self.map[self.max_frequency].pop()
-        if not self.map[self.max_frequency]:
+        result = self.board[self.max_frequency].pop()
+        self.counter[result] -= 1
+
+        if not self.board[self.max_frequency]:
             self.max_frequency -= 1
 
-        self.frequency[target] -= 1
-
-        return target
+        return result
