d167a0b21006dbdf12236e8fa17e484105d60991
Seokjae Lee <seok9211@naver.com>
2024-07-20 18:02:39 +0900

    Time: 489 ms (64.71%), Space: 31.9 MB (36.94%) - LeetHub

--- 3207-maximum-points-after-enemy-battles/3207-maximum-points-after-enemy-battles.py
+++ 3207-maximum-points-after-enemy-battles/3207-maximum-points-after-enemy-battles.py
@@ -3,24 +3,14 @@ from typing import List
 
 class Solution:
     def maximumPoints(self, enemyEnergies: List[int], currentEnergy: int) -> int:
+        mininum = float("inf")
+        totalEnergy = currentEnergy
+        
+        for energy in enemyEnergies:
+            mininum = min(mininum, energy)
+            totalEnergy += energy
 
-        enemyEnergies.sort()
-        points = 0
-        mark = set()
-
-        if currentEnergy <= 0:
+        if mininum > currentEnergy:
             return 0
-        if currentEnergy < enemyEnergies[0]:
-            return 0
 
-        right = len(enemyEnergies) - 1
-
-        while right >= 0:
-            if currentEnergy >= enemyEnergies[0]:
-                points += currentEnergy // enemyEnergies[0]
-                currentEnergy = currentEnergy % enemyEnergies[0]
-            else:
-                currentEnergy += enemyEnergies[right]
-                right -= 1
-
-        return points
+        return (totalEnergy - mininum) // mininum
