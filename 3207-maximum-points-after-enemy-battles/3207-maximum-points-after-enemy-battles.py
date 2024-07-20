077bdcf83ba4c9706e3cf8d627e12e938277087b
Seokjae Lee <seok9211@naver.com>
2024-07-20 18:00:52 +0900

    Time: 544 ms (34.73%), Space: 31.8 MB (80.32%) - LeetHub

--- 3207-maximum-points-after-enemy-battles/3207-maximum-points-after-enemy-battles.py
+++ 3207-maximum-points-after-enemy-battles/3207-maximum-points-after-enemy-battles.py
@@ -3,9 +3,6 @@ from typing import List
 
 class Solution:
     def maximumPoints(self, enemyEnergies: List[int], currentEnergy: int) -> int:
-        # 처음에 가장 적은 에너지를 가진 애의 애너지를 빼고 포인트를 얻어야 함
-        # 그 다음에 가장 큰 에너지를 가진 애를 마킹하고 애너지를 더해야 함
-        # 이걸 반복?
 
         enemyEnergies.sort()
         points = 0
@@ -13,10 +10,12 @@ class Solution:
 
         if currentEnergy <= 0:
             return 0
+        if currentEnergy < enemyEnergies[0]:
+            return 0
 
         right = len(enemyEnergies) - 1
 
-        while right:
+        while right >= 0:
             if currentEnergy >= enemyEnergies[0]:
                 points += currentEnergy // enemyEnergies[0]
                 currentEnergy = currentEnergy % enemyEnergies[0]
