9c0fa43b44a5e417afaa81825a43346099a28c5a
Seokjae Lee <seok9211@naver.com>
2025-02-09 11:20:09 +0900

    Time: 11 ms (31.02%), Space: 19.4 MB (47.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,3 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return Counter(nums).most_common(1)[0][0]
+        cnt = 0
+        result = float("inf")
+        for num in nums:
+            if result == float("inf"):
+                result, cnt = num, 1
+            else:
+                if result == num:
+                    cnt += 1
+                else:
+                    cnt -= 1
+                    if cnt < 0:
+                        result = num
+                        cnt = 1
+
+        return result
